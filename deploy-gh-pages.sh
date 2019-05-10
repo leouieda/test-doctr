1bb2904f1c0db6c93736c5c4b063c0719b39cdcf
Leonardo Uieda <leouieda@gmail.com>
2019-05-09 15:39:56 -1000

    First stab at doctr

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -10,70 +10,55 @@
 set -e
 set -x
 
-REPO=$TRAVIS_REPO_SLUG
-BRANCH=gh-pages
-CLONE_DIR=deploy
-CLONE_ARGS="--quiet --branch=$BRANCH --single-branch"
-REPO_URL=https://${GH_TOKEN}@github.com/${REPO}.git
-HTML_SRC=${TRAVIS_BUILD_DIR}/${HTML_BUILDDIR:-doc/_build/html}
 # Place the HTML is different folders for different versions
 if [[ "${TRAVIS_TAG}" != "" ]]; then
     VERSION=${TRAVIS_TAG}
 else
     VERSION=dev
 fi
+SOURCE=${HTML_BUILDDIR:-doc/_build/html}
+DESTINATION=${VERSION}
 
 echo -e "DEPLOYING HTML TO GITHUB PAGES:"
-echo -e "Target: branch ${BRANCH} of ${REPO}"
-echo -e "HTML source: ${HTML_SRC}"
-echo -e "HTML destination: ${VERSION}"
+echo -e "HTML source: ${SOURCE}"
+echo -e "HTML destination: ${DESTINATION}"
 
-# Clone the project, using the secret token.
-# Uses /dev/null to avoid leaking decrypted key.
-echo -e "Cloning ${REPO}"
-git clone ${CLONE_ARGS} ${REPO_URL} ${CLONE_DIR} 2>&1 >/dev/null
 
-cd ${CLONE_DIR}
+cat > ${SOURCE}/process.sh << EOF
+    # Need to have this file so that Github doesn't try to run Jekyll
+    touch .nojekyll
+    touch test-file
 
-# Configure git to a dummy Travis user
-git config user.email "travis@nothing.com"
-git config user.name "TravisCI"
+    # If this is a new release, update the link from /latest to the new release
+    if [[ "${VERSION}" != "dev" ]]; then
+        echo -e "Setup link from ${VERSION} to 'latest'"
+        rm -f latest
+        ln -sf ${DESTINATION} latest
+    fi
 
-# Delete all the files and replace with our new  set
-echo -e "Remove old files from previous builds"
-rm -rf ${VERSION}
-cp -Rf ${HTML_SRC}/ ${VERSION}/
+    echo -e "Add and commit changes"
+    git add -A .
+    git status
+    # If this is a dev build and the last commit was from a dev build, reuse the
+    # same commit
+    if [[ "${VERSION}" == "dev" && `git log -1 --format='%s'` == *"dev"* ]]; then
+        echo -e "Amending last commit"
+        git commit --amend --reset-author --no-edit
+    else
+        # Make a new commit
+        echo -e "Making a new commit"
+        git commit -m "Deploy $VERSION from TravisCI"
+    fi
+EOF
 
-# Need to have this file so that Github doesn't try to run Jekyll
-touch .nojekyll
-
-# If this is a new release, update the link from /latest to the new release
-if [[ "${VERSION}" != "dev" ]]; then
-    echo -e "Setup link from ${VERSION} to 'latest'"
-    rm -f latest
-    ln -sf ${VERSION} latest
-fi
-
-echo -e "Add and commit changes"
-git add -A .
-git status
-# If this is a dev build and the last commit was from a dev build, reuse the
-# same commit
-if [[ "${VERSION}" == "dev" && `git log -1 --format='%s'` == *"dev"* ]]; then
-    echo -e "Amending last commit"
-    git commit --amend --reset-author --no-edit
-else
-    # Make a new commit
-    echo -e "Making a new commit"
-    git commit -m "Deploy $VERSION from TravisCI"
-fi
-
-echo -e "Pushing..."
-git push -fq origin $BRANCH 2>&1 >/dev/null
+doctr deploy \\
+    ${VERSION} \\
+    --build-tags \\
+    --built-docs ${DESTINATION} \\
+    --key-path .github/deploy_key.enc \\
+    --command "./process.sh"
 
 echo -e "Finished uploading generated files."
 
-# Workaround for https://github.com/travis-ci/travis-ci/issues/6522
-# Turn off exit on failure.
 set +x
 set +e
