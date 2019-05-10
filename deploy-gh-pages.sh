9fab2b42c4ae43ff9fc4f525a48ccc5c4d1a54ef
Leonardo Uieda <leouieda@gmail.com>
2019-05-09 16:30:38 -1000

    Fix location of process.sh

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -26,33 +26,40 @@ echo -e "HTML destination: ${DESTINATION}"
 
 
 cat > ${SOURCE}/process.sh << EOF
-    # Need to have this file so that Github doesn't try to run Jekyll
-    touch .nojekyll
-    touch test-file
+#!/bin/bash
+set -o xtrace
+set -e
+set -x
 
-    # If this is a new release, update the link from /latest to the new release
-    if [[ "${VERSION}" != "dev" ]]; then
-        echo -e "Setup link from ${VERSION} to 'latest'"
-        rm -f latest
-        ln -sf ${DESTINATION} latest
-    fi
+# Need to have this file so that Github doesn't try to run Jekyll
+touch .nojekyll
+touch test-file
 
-    echo -e "Add and commit changes"
-    git add -A .
-    git status
-    # If this is a dev build and the last commit was from a dev build, reuse the
-    # same commit
-    if [[ "${VERSION}" == "dev" && `git log -1 --format='%s'` == *"dev"* ]]; then
-        echo -e "Amending last commit"
-        git commit --amend --reset-author --no-edit
-    else
-        # Make a new commit
-        echo -e "Making a new commit"
-        git commit -m "Deploy $VERSION from TravisCI"
-    fi
+# If this is a new release, update the link from /latest to the new release
+if [[ "${VERSION}" != "dev" ]]; then
+    echo -e "Setup link from ${VERSION} to 'latest'"
+    rm -f latest
+    ln -sf ${DESTINATION} latest
+fi
+
+echo -e "Add and commit changes"
+git add -A .
+git status
+# If this is a dev build and the last commit was from a dev build, reuse the
+# same commit
+if [[ "${VERSION}" == "dev" && `git log -1 --format='%s'` == *"dev"* ]]; then
+    echo -e "Amending last commit"
+    git commit --amend --reset-author --no-edit
+else
+    # Make a new commit
+    echo -e "Making a new commit"
+    git commit -m "Deploy $VERSION from TravisCI"
+fi
 EOF
 
-doctr deploy ${DESTINATION} --build-tags --built-docs ${SOURCE} --key-path .github/deploy_key.enc --command "./process.sh"
+chmod +x ${SOURCE}/process.sh
+
+doctr deploy ${DESTINATION} --build-tags --built-docs ${SOURCE} --key-path .github/deploy_key.enc --command "${DESTINATION}/process.sh"
 
 echo -e "Finished uploading generated files."
 
